6ac3ee24cacbcd90ad21d1308d817aeeda40bdd9
Grymyl <russian.bogd@gmail.com>
2021-04-16 16:55:25 +0300

    Added anchor, but no smooth transition. Added sticky menu without scrolling and when entering the site

--- script/script.js
+++ script/script.js
@@ -1,18 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const nav = document.querySelector('.nav');
+    const nav = document.querySelector('.nav'),
+          buttonViewWork = document.querySelector('.button_view_work'),
+          about = document.querySelector('.about');
     
     function navFixed() {
         let a = window.innerHeight;
         let wpYO = window.pageYOffset;
-        console.log(nav.classList);
+        //console.log(nav.classList);
         if (wpYO >= a) {
-            console.log('a');
+            //console.log('a');
             
             nav.classList.add('fixed')
         } else {
             nav.classList.remove('fixed')
         }
     }
+    function viewWork() {
+        let clY = about.getBoundingClientRect().y
+        //console.dir(clY);
+        window.scrollBy(0, clY)
+    }
+
+    navFixed()
 
     document.addEventListener('scroll', navFixed)
+    buttonViewWork.addEventListener('click', viewWork)
 })
