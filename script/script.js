c99abd2e850def79fa73acc2724060b211fa9e98
Grymyl <russian.bogd@gmail.com>
2021-04-27 09:26:12 +0300

    Added pop-up frames when scrolling the page. Works only on personal computers. Not for phones.

--- script/script.js
+++ script/script.js
@@ -4,15 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
           navSite = document.querySelector('.nav-site'),
           linkSite = document.querySelector('.link-site'),
           burgerButton= document.querySelector('.burger-button'),
-          about2 = document.querySelector('.about2')
+          about2 = document.querySelector('.about2'),
+          framesH = document.querySelectorAll('.frame-h'),
+          framesV = document.querySelectorAll('.frame-v')
     
     //функция фиксации меню при прокрутке
     const objectPos = navSite.offsetTop
     function scrollPage () {
-        let wpYO = window.pageYOffset;
-        let a = window.innerHeight;
+        let scrollTop = window.pageYOffset;
+        let windowHeight = window.innerHeight;
+
         function navFixed() {
-            if (wpYO >= a) {
+            if (scrollTop >= windowHeight) {
                 nav.classList.add('fixed-l')
             } else {
                 nav.classList.remove('fixed-l')
@@ -21,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
         navFixed()
 
         function objectFixed() {
-            if (wpYO >= objectPos) {
+            if (scrollTop >= objectPos) {
                 navSite.classList.add('fixed-r')
             } else {
                 navSite.classList.remove('fixed-r')
@@ -31,46 +34,74 @@ document.addEventListener('DOMContentLoaded', () => {
         objectFixed()
 
         function frameworks () {
-            if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { 
                 //console.log("ПК");
                 //console.dir(about2);
                 //console.log(about2.clientHeight); 
                 //console.log("н", about2.offsetTop); // точка начала блока
                 //console.log(about2.scrollHeight);
-                //console.log("нп", wpYO+window.innerHeight); // начало блока с прокруткой
-                if (wpYO + window.innerHeight >= about2.offsetTop && wpYO+window.innerHeight <= about2.clientHeight + about2.offsetTop ) {
-                    let centerObj = about2.clientHeight / 2
-                    //console.log('ценрт объекта', centerObj);
-                    //console.log(wpYO+window.innerHeight - about2.offsetTop)
-                    if (centerObj <= wpYO+window.innerHeight - about2.offsetTop) {
-                        let a1 = Math.floor(centerObj),
-                            a2 = wpYO+window.innerHeight,
+                //console.log("нп", scrollTop+windowHeight); // начало блока с прокруткой
+                if (scrollTop + windowHeight >= about2.offsetTop && scrollTop+windowHeight <= about2.clientHeight + about2.offsetTop ) {
+                    let middleObject = Math.round( about2.clientHeight / 2 )
+                    
+                    //console.log('ценрт объекта', middleObject);
+                    //console.log(scrollTop+windowHeight - about2.offsetTop)
+
+                    if (middleObject <= scrollTop + windowHeight - about2.offsetTop) {
+                        let a1 = middleObject,
+                            a2 = scrollTop + windowHeight,
                             a3 = about2.offsetTop,
                             a4 = a2 - a3,
-                            a5 = (a4) - a1
+                            a5 = ( a4 ) - a1,
+                            frameTransMatrix = Math.round ( a5 * ( 999999 / a1 ) )
 
-                        console.log('рамки');
-                        console.log("centerObj", a1);
-                        console.log("wpYO+window.innerHeight ",  a2);
+                        /*console.log('рамки');
+                        console.log("middleObject", a1);
+                        console.log("scrollTop+windowHeight ",  a2);
                         console.log("about2.offsetTop", a3);
-                        console.log("wpYO+window.innerHeight - about2.offsetTop", a4);
-                        console.log("(wpYO+window.innerHeight - about2.offsetTop) - centerObj", a5);
+                        console.log("scrollTop+windowHeight - about2.offsetTop", a4);
+                        console.log("(scrollTop+windowHeight - about2.offsetTop) - middleObject", a5);*/
 
-                        let framesH = document.querySelectorAll('.frame-h'),
-                            framesV = document.querySelectorAll('.frame-v')
                         for (let i = 0; i < framesH.length; i++){
-                            framesH[i].style.transform = `matrix(1, 0, 0, .${a5}, 0, 0)`
+                            if (frameTransMatrix < 100000) {
+                                framesH[i].style.transform = `matrix(1, 0, 0, .0${frameTransMatrix}, 0, 0)`
+                            } else {
+                                framesH[i].style.transform = `matrix(1, 0, 0, .${frameTransMatrix}, 0, 0)`
+                            }
                         }
+
                         for (let i = 0; i < framesV.length; i++){
-                            framesV[i].style.transform = `matrix(.${a5}, 0, 0, 1, 0, 0)`
+                            if (frameTransMatrix < 100000) {
+                                framesV[i].style.transform = `matrix(.0${frameTransMatrix}, 0, 0, 1, 0, 0)`
+                            } else {
+                                framesV[i].style.transform = `matrix(.${frameTransMatrix}, 0, 0, 1, 0, 0)`
+                            }
                         }
+                    } else if (middleObject > scrollTop + windowHeight - about2.offsetTop) {
+                        //console.log("выше центра");
+                        for (let i = 0; i < framesH.length; i++){
+                            framesH[i].style.transform = `matrix(1, 0, 0, 0, 0, 0)`
+                        }
+    
+                        for (let i = 0; i < framesV.length; i++){
+                            framesV[i].style.transform = `matrix(0, 0, 0, 1, 0, 0)`
+                        }
+                    }
+                } else if (scrollTop + windowHeight < about2.offsetTop) {
+                    //console.log("Выше самой секции но не центра");
+                    
+                } else if (scrollTop+windowHeight > about2.clientHeight + about2.offsetTop) {
+                    //console.log("Ниже");
+                    for (let i = 0; i < framesH.length; i++){
+                        framesH[i].style.transform = `matrix(1, 0, 0, 1, 0, 0)`
+                    }
 
-                    } 
-
+                    for (let i = 0; i < framesV.length; i++){
+                        framesV[i].style.transform = `matrix(1, 0, 0, 1, 0, 0)`
+                    }
                 }
-
             } else {
-                console.log("Мобил");
+                //console.log("Мобил");
             }  
         }
         frameworks ()
